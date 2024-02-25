b6b2478ab68cf790205f1d664417d9cf26c28ac4
Hamad Dwairi <98936288+hamadbd@users.noreply.github.com>
2024-02-25 23:12:54 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    // toggle mobile menu
+    $('[data-toggle="toggle-nav"]').on('click', function () {
+        $(this).closest('nav').find($(this).attr('data-target')).toggleClass('hidden');
+        return false;
+    });
+
+    // feather icons
+    feather.replace();
+
+    // smooth scroll
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+    // tiny slider
+    $('#slider-1').slick({
+        infinite: true,
+        speed: 1000,  // Adjust the speed (in milliseconds) for a slower transition
+        autoplay: true,
+        autoplaySpeed: 750,  // Adjust the autoplay speed (in milliseconds)
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+    });
+
+    // tiny slider
+    $('#slider-10').slick({
+        infinite: true,
+        speed: 1000,  // Adjust the speed (in milliseconds) for a slower transition
+        autoplay: true,
+        autoplaySpeed: 750,  // Adjust the autoplay speed (in milliseconds)
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+    });
+
+    $('#slider-2').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        customPaging: function (slider, i) {
+            return '<div class="bg-white br-round w-1 h-1 opacity-50 mt-5" id=' + i + '> </div>'
+        },
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, ]
+    });
+});
